bd1e63d3f299480b4f6f55d41bc116354b498d59
Shubham Srivastava <codetosuccessyt@gmail.com>
2026-07-06 01:05:18 +0530

    started

---
+++ backend/core/base/tools/tool.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+import inspect
+from typing import Callable,Any
+
+
+class Tool(ABC):
+
+    def __init__(self,name,description,func):
+        self.name : str = name
+        self.description : str = description
+        self.func : Callable[...,Any] = func
+        self.signature = inspect.signature(self.func)
+
+    @abstractmethod
+    def run(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def to_model_specific(self):
+        pass
